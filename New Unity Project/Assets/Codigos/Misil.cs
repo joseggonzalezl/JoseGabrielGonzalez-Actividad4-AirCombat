2010522ee340cabedeb78a017566201a57e8151c
joseggonzalezl <josegonzalez9616@hotmail.com>
2018-04-14 21:18:46 -0500

    Version 1 Actividad 4

---
+++ New Unity Project/Assets/Codigos/Misil.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misil : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Translate (0.0f, 0.0f, 2.0f);
+	}
+
+	void OnCollisionEnter (Collision collision) {
+
+
+		Destroy (collision.collider.gameObject);
+
+		Destroy (this.gameObject);
+	}
+}
---
+++ New Unity Project/Assets/Codigos/Mover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+
+			this.transform.Translate (0.0f, 1.5f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+
+			this.transform.Translate (0.0f, -1.5f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+
+			this.transform.Translate (0.0f, 0.0f, -1.5f);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+
+			this.transform.Translate (0.0f, 0.0f, 1.5f);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)){
+
+			GameObject Misil = GameObject.Instantiate (Resources.Load ("prefab/Misil") as GameObject);
+			Misil.transform.SetParent (this.gameObject.transform);
+			Misil.transform.localPosition = new Vector3 (0.14f, 1.03f, 0.62f);
+			Misil.transform.SetParent (null);
+	}
+}
+}
